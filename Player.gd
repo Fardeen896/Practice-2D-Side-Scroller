03a6d9855baa54dc94e192b30d54715a0f471771
Fardeen Hossain <fardeen.hossain896@gmail.com>
2022-02-16 23:41:20 -0500

    Added several animations/sprites and implemented basic movement

---
+++ Player.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+const MOVEMENT_SPEED = 100
+const GRAVITY = 25
+const JUMP_POWER = -300
+const FLOOR = Vector2(0, -1)
+
+var velocity = Vector2()
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -MOVEMENT_SPEED
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = MOVEMENT_SPEED
+	else:
+		velocity.x = 0
+	
+	if Input.is_action_pressed("ui_up"):
+		velocity.y = -MOVEMENT_SPEED
+	else: 
+		velocity.y = 0
+	
+	move_and_slide(velocity)
