cd1f610e304e4262a0bac673ac6b2e962386a63e
Fardeen Hossain <fardeen.hossain896@gmail.com>
2022-02-18 15:01:00 -0500

    Tweaked player code to play animations correctly

--- Player.gd
+++ Player.gd
@@ -2,23 +2,57 @@ extends KinematicBody2D
 
 const MOVEMENT_SPEED = 100
 const GRAVITY = 25
-const JUMP_POWER = -300
+const JUMP_POWER = -450
 const FLOOR = Vector2(0, -1)
 
 var velocity = Vector2()
+var on_ground = false
+var is_attacking = false
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -MOVEMENT_SPEED
+		$AnimatedSprite.flip_h = true
+		if is_attacking == false:
+			$AnimatedSprite.play("run")
 	elif Input.is_action_pressed("ui_right"):
 		velocity.x = MOVEMENT_SPEED
+		$AnimatedSprite.flip_h = false
+		if is_attacking == false:
+			$AnimatedSprite.play("run")
 	else:
 		velocity.x = 0
+		if on_ground == true && is_attacking == false:
+			$AnimatedSprite.play("idle")
 	
 	if Input.is_action_pressed("ui_up"):
-		velocity.y = JUMP_POWER
-		
-		
+		if on_ground == true:
+			velocity.y = JUMP_POWER
+			on_ground = false
+			
+	
 	velocity.y += GRAVITY
 	
-	velocity = move_and_slide(velocity)
+	if Input.is_action_just_pressed("ui_focus_next") && is_attacking == false:
+		#attack
+		is_attacking = true
+		$AnimatedSprite.play("attack")
+		
+	
+	
+	if is_on_floor():
+		on_ground = true
+	else:
+		on_ground = false
+		if is_attacking == false:
+			if velocity.y < 0:
+				$AnimatedSprite.play("jump")
+			else:
+				$AnimatedSprite.play("fall")
+		
+	
+	velocity = move_and_slide(velocity, FLOOR)
+
+
+func _on_AnimatedSprite_animation_finished():
+	is_attacking = false
