950b5cac227743b982cd49c44b942c8851341b92
Fardeen Hossain <fardeen.hossain896@gmail.com>
2022-03-22 23:08:15 -0400

    Added tooltips, fixed some bugs

--- Slot.gd
+++ Slot.gd
@@ -1,10 +1,12 @@
 extends Panel
 
 var ItemClass = preload("res://Item.tscn")
+var ToolTipClass = preload("res://ToolTip.tscn")
 var item = null
 var slot_index
 
 func _ready():
+	
 	pass
 	#if randi() % 2 == 0:
 		#item = ItemClass.instance()
@@ -30,3 +32,21 @@ func initialize_item(item_name, item_quantity):
 		item.set_item(item_name, item_quantity)
 	else:
 		item.set_item(item_name, item_quantity)
+
+func _on_Slot1_mouse_entered():
+	var toolTipInstance = ToolTipClass.instance()
+	toolTipInstance.rect_position = get_global_transform_with_canvas().origin - Vector2(300,0)
+	
+	add_child(toolTipInstance)
+	yield(get_tree().create_timer(0.1), "timeout")
+	if has_node("ToolTip") && has_node("Item"):
+		var slotToolTip = get_node("ToolTip")
+		var itemName = get_node("Item").item_name
+		slotToolTip.setItemName(itemName)
+		#JsonData.item_data[item_name]["StackSize"]
+		slotToolTip.setDescription(JsonData.item_data[itemName]["Description"])
+		slotToolTip.show()
+
+func _on_Slot1_mouse_exited():
+	get_node("ToolTip").free()
+
--- UserInterface.gd
+++ UserInterface.gd
@@ -8,3 +8,4 @@ func _input(event):
 	if event.is_action_pressed("inventory"):
 		$Inventory.visible = !$Inventory.visible
 		$Inventory.initialize_inventory()
+		get_tree().paused = !get_tree().paused
